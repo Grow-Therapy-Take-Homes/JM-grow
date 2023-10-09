48970096f3087b928a5702c861b883d0db0daed0
me <me@mes-macbook-pro.lan>
2023-10-09 09:54:19 -0400

    remove dead code

--- src/utils/step.tsx
+++
@@ -1,4 +0,0 @@
-export const step = (times, step, start = 0) => {
-  //create a function that takes a number and creates an array with increments
-  return Array.from({ length: times }, (_, i) => (i + 1) * step + start);
-};
